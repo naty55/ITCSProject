de5359ead1f61e47438ef8daa9f9ee6e5785865d
naty55 <64107292+naty55@users.noreply.github.com>
2024-12-30 23:55:59 +0200

    server - register process

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 import socket
 from threading import Thread
 from config import SERVER_HOST, SERVER_PORT
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import serialization,hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 import resources
+import utils
 from importlib.resources import read_binary
 
 class Server:
@@ -18,20 +20,63 @@ class Server:
             s.listen(10)
             while True:
                 conn, addr = s.accept()
-                print("Accepted")
-                Thread(target=Server.handle_connection, args=(conn,)).start()
+                print(f"Accepted connection from {addr}")
+                Thread(target=self.handle_new_connection, args=(conn,)).start()
     
-    def handle_connection(conn):
+    def handle_new_connection(self, conn):
+        data = conn.recv(4096)
+        header, payload = data.split(b'\n\n', 1)
+        req_type, client_id = header.split(b' ')
+        if req_type == b'register':
+            self.handle_register_request(conn)
+        elif req_type == b'connect':
+            if client_id not in self.clients or not self.clients[client_id]['registerd']:
+                raise
+
+        # Connection stays open for next requests
         while True:
-            print("Reading data from socket")
             data = conn.recv(4096)
             if not data:
+                self.clients[client_id]['has_session'] = False
                 break
-            print(data)
+            self.handle_message_request(data)
         
+    def handle_register_request(self, conn, client_id, payload):
+        client_id = client_id.decode()
+        if not utils.validate_client_id(client_id):
+            return False
+        print(f"Client {client_id} is trying to register")
+        new_client = dict()
+        self.clients[client_id] = new_client
+        new_client['public_key'] = serialization.load_pem_public_key(payload)
+        self.send_by_secure_channel(client_id, conn)
+        data = conn.recv(4096)
+        
+        header, payload = data.split(b'\n\n', 1)
+        req_type, otc = header.split(b' ')
+        if req_type != b'verify_otc':
+            raise
+        print(f"Recieved otc from client, client id {client_id}, otc={otc}")
+        registration_failed = False
+        if otc.decode() != new_client['otc']:
+            registration_failed = True
+        else:
+            if not utils.verify_signature(new_client['public_key'], otc, payload):
+                registration_failed = True
+            new_client['registerd'] = True
+            new_client['has_session'] = True
+        if registration_failed:
+            del self.clients[client_id]
 
-    def send_by_secure_channel(phone_number, conn):
-        pass
+        return not registration_failed
+
+    def handle_message_request(self, request_bytes):
+        print("got message", request_bytes)
+
+    def send_by_secure_channel(self, client_id, conn):
+        otc = utils.generate_otc()
+        self.clients[client_id]['otc'] = otc
+        conn.send(bytes(otc,"utf-8"))
 
     def read_private_key():
         private_key = serialization.load_pem_private_key(
--- server/utils.py
+++ server/utils.py
@@ -1,4 +1,22 @@
+from cryptography.hazmat.primitives import serialization,hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.exceptions import InvalidSignature
+import random
+
 valid_phone_numbers = {str(i) * 9 for i in range(10)} # phone number is of pattern "ddddddddd" e.g. "111111111"
 
-def validate_phone_number(phone_number):
-    return phone_number in valid_phone_numbers
+def validate_client_id(client_id):
+    return client_id in valid_phone_numbers
+
+def generate_otc():
+    return ''.join(random.choices('0123456789', k=6))
+
+def verify_signature(public_key, message, signature):
+    try:
+        public_key.verify(signature, message, padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH),
+            hashes.SHA256())
+        return True
+    except InvalidSignature:
+        return False
